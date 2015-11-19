a3f9ff06302e222a0459dfe0e7c400d6a9f202ba
Jonathan Viccary <jonathan.viccary@gmail.com>
2015-11-18 22:29:14 -0500

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bohm
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bohm/core_test.clj
@@ -0,0 +1,7 @@
+(ns bohm.core-test
+  (:require [clojure.test :refer :all]
+            [bohm.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
