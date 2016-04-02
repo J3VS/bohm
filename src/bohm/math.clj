762a220b9744b46364a51c4a0dad93ce9c6eb5c9
Jonathan Viccary <jonathan.viccary@gmail.com>
2016-04-02 10:27:31 -0400

    Added determinant

--- src/bohm/math.clj
+++ src/bohm/math.clj
@@ -30,13 +30,17 @@
     (when (every? #(= first-col (count %)) x)
       first-col)))
 
+(defn mval
+  [x i j]
+  (get-in x [i j]))
+
 (defn mmult-entry
   [a b row-index column-index]
   (->> a
        columns
        range
-       (mapv #(* (get-in a [row-index %])
-                (get-in b [% column-index])))
+       (mapv #(* (mval a row-index %)
+                 (mval b % column-index)))
        (reduce +)))
 
 (defn matrix-skeleton
@@ -58,27 +62,59 @@
   [x y]
   (when (equal-size x y)
     (matrix-skeleton (fn [i j]
-                       (+ (get-in x [i j]) (get-in y [i j]))) (rows x) (columns x))))
+                       (+ (mval x i j) (mval y i j))) (rows x) (columns x))))
 
 (defn msubtract
   [x y]
   (when (equal-size x y)
     (matrix-skeleton (fn [i j]
-                       (- (get-in x [i j]) (get-in y [i j]))) (rows x) (columns x))))
+                       (- (mval x i j) (mval y i j))) (rows x) (columns x))))
 
 
 (defn transpose
   [x]
   (if-let [cols (columns x)]
     (matrix-skeleton (fn [i j]
-                       (get-in x [j i])) cols (rows x))))
+                       (mval x j i)) cols (rows x))))
 
 
 (defn square?
   [x]
-  (= (rows x) (columns x)))
+  (let [r (rows x)
+        c (columns x)]
+    (when (= r c)
+      [r c])))
+
+(defn remove-row
+  [x r]
+  (matrix-skeleton (fn [i j]
+                     (if (< i r)
+                       (mval x i j)
+                       (mval x (+ i 1) j))) (- (rows x) 1) (columns x)))
+
+(defn remove-column
+  [x c]
+  (matrix-skeleton (fn [i j]
+                     (if (< j c)
+                       (mval x i j)
+                       (mval x i (+ j 1)))) (rows x) (- (columns x) 1)))
+
+(defn mrest
+  [x r c]
+  (-> x
+      (remove-row r)
+      (remove-column c)))
 
 (defn determinant
   [x]
-  (let [row-count (rows x)
-        col-count (columns x)]))
+  (if-let [[r c] (square? x)]
+    (if (= r c 2)
+      (- (* (mval x 0 0) (mval x 1 1))
+         (* (mval x 1 0) (mval x 0 1)))
+      (->> c
+           range
+           (map (fn [col]
+                  (* (mval x 0 col) (Math/pow -1 col) (determinant (mrest x 0 col)))))
+           (apply +)))))
+
+
